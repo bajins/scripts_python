7c12fa6356ff76a717612de22d7609c04d4d507c
bajins <admin@bajins.com>
2019-12-26 14:41:07 +0800

    update

--- utils/ReptileUtil.py
+++ utils/ReptileUtil.py
@@ -11,13 +11,12 @@
 
 import os
 import sys
-import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver import DesiredCapabilities
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.wait import WebDriverWait
+# from selenium.webdriver.support import expected_conditions
 
 from utils import HttpUtil, FileUtil
 
