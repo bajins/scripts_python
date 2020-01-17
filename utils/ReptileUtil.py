07dfc03d3ad9b510e844767f8423995560724c97
bajins <admin@bajins.com>
2020-01-17 17:03:09 +0800

    更新参数

--- utils/ReptileUtil.py
+++ utils/ReptileUtil.py
@@ -21,9 +21,11 @@ import sys
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver import DesiredCapabilities
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.wait import WebDriverWait
-# from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+from selenium.webdriver.support.wait import WebDriverWait
 
 from utils import HttpUtil, FileUtil, SystemUtil
 
@@ -122,70 +124,62 @@ def selenium_driver(url, debug=False):
 
     # chrome选项
     options = webdriver.ChromeOptions()
-    options.add_argument(f'--user-agent={HttpUtil.USER_AGENT}')
+    # 手动指定使用的浏览器位置
+    # options.binary_location = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+    options.add_argument(f'-user-agent={HttpUtil.USER_AGENT}')
     # 启动即窗口最大化
-    options.add_argument("--start-maximized")
-    # 关闭"chrome正受到自动测试软件的控制"提示
-    options.add_argument("disable-infobars")
+    options.add_argument("-start-maximized")
+    # 关闭"chrome正受到自动测试软件的控制"提示和控制台打印DevTools listening
+    options.add_experimental_option("excludeSwitches", ['enable-automation', 'enable-logging'])
+    # 禁止加载所有插件，可以增加速度
+    options.add_argument('–disable-plugins')
+    # 禁用扩展
+    options.add_argument('-disable-extensions')
+    # 禁用浏览器应用
+    options.add_argument('-disable-software-rasterizer')
+    # 忽略证书错误
+    options.add_argument('-ignore-certificate-errors')
+    prefs = {
+        # 不加载图片
+        "profile.managed_default_content_settings.images": 2,
+        'profile.default_content_settings.popups': 0,
+        # 默认下载地址
+        'download.default_directory': 'D:\\',
+        # 下载不弹窗
+        'download.prompt_for_download': False,
+        'download.directory_upgrade': True,
+        'safebrowsing.enabled': False,
+        'safebrowsing.disable_download_protection': True,
+        'profile.default_content_setting_values': {
+            # 关掉浏览器左上角的通知提示
+            'notifications': 2,
+            # 禁用自动下载
+            "automatic_downloads": 0
+        }
+    }
     if not debug:
         # 设置chrome浏览器无界面模式
-        options.add_argument('--headless')
+        options.add_argument('-headless')
         # 谷歌文档提到需要加上这个属性来规避bug
-        options.add_argument('--disable-gpu')
+        options.add_argument('-disable-gpu')
         # 隐身模式启动
         options.add_argument('-–incognito')
         # 取消沙盒模式
-        options.add_argument('--no-sandbox')
+        options.add_argument('-no-sandbox')
         # 指定浏览器分辨率
-        options.add_argument('window-size=1600x900')
-        # 禁止加载所有插件，可以增加速度
-        options.add_argument('-–disable-plugins')
-        # 禁用扩展
-        options.add_argument('--disable-extensions')
-        # 隐藏滚动条, 应对一些特殊页面
-        options.add_argument('--hide-scrollbars')
-        # 这种方式在Headless的模式下是生效的， 非Headless模式下也是生效的。不加载图片, 提升速度
-        # options.add_argument('blink-settings=imagesEnabled=false')
-        options.add_argument('-–disable-images')
-        # 手动指定使用的浏览器位置
-        # options.binary_location = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
-        # 添加crx插件
-        # options.add_extension('d:\crx\AdBlock_v2.17.crx')
-        # 禁用JavaScript
-        # options.add_argument("--disable-javascript")
-        # 设置开发者模式启动，该模式下webdriver属性为正常值
-        # options.add_experimental_option('excludeSwitches', ['enable-automation'])
+        options.add_argument('-window-size=1600x900')
 
-        # 用于定义下载不弹窗和默认下载地址（默认下载地址还要再后面的commands里启动，默认是不开启的）
-        prefs = {
-            "profile.managed_default_content_settings.images": 2,
-            'profile.default_content_settings.popups': 0,
-            'download.default_directory': 'D:\\',
-            'download.prompt_for_download': False,
-            'download.directory_upgrade': True,
-            'safebrowsing.enabled': False,
-            'safebrowsing.disable_download_protection': True,
-            'profile.default_content_setting_values': {
-                # 关掉浏览器左上角的通知提示
-                'notifications': 2,
-                "automatic_downloads": 0
-            }
-        }
-        options.add_experimental_option('prefs', prefs)
-    capa = DesiredCapabilities.CHROME
+    options.add_experimental_option('prefs', prefs)
+    # capa = DesiredCapabilities.CHROME
+    capa = options.to_capabilities()
     # 懒加载模式，不等待页面加载完毕
     # capa["pageLoadStrategy"] = "none"
 
     # 打开浏览器,executable_path指定驱动位置
-    driver = webdriver.Chrome(chrome_options=options, executable_path=path, desired_capabilities=capa)
-
-    # 向Selenium Webwdriver添加对Chrome "send_command"的支持
-    driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
-    # 禁用浏览器下载
-    # https://github.com/TheBrainFamily/chimpy/issues/108#issuecomment-512836924
-    # allow自动、deny禁止、default默认
-    params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'deny', 'downloadPath': "D:\\"}}
-    driver.execute("send_command", params)
+    # driver = webdriver.Chrome(chrome_options=options, executable_path=path, desired_capabilities=capa)
+    driver = webdriver.Chrome(executable_path=path, desired_capabilities=capa)
+    # 下载设置
+    driver.execute_cdp_cmd("Page.setDownloadBehavior", {'behavior': 'deny', 'downloadPath': "D:\\"})
 
     # 在启动浏览器并打开页面后，最大化浏览器
     # driver.maximize_window()
@@ -205,6 +199,22 @@ def selenium_driver(url, debug=False):
     driver.get(url)
 
     return driver
+
+
+def is_element_present(driver, by, value):
+    """
+    用来判断元素标签是否存在，
+    selenium.common.exceptions
+    """
+    try:
+        driver.find_element(by=by, value=value)
+    except Exception as e:
+        print(e)
+        # 发生了NoSuchElementException异常，说明页面中未找到该元素，返回False
+        return False
+    else:
+        # 没有发生异常，表示在页面中找到了该元素，返回True
+        return True
 
 
 if __name__ == '__main__':
